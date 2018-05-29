9b80fca35a7a5c92b5b7fdc250bc5cd29174bfba
Shanelle Savoy <mznellienellz@gmail.com>
2018-05-29 00:30:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
-var kittens =["Milo", "Otis", "Garfield"]
+kittens =["Milo", "Otis", "Garfield"]
 
-array destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(name) {
   kittens.push()
   return array
 }
